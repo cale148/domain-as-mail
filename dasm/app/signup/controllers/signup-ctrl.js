641cbdc7a132bc2bb8e8b32a4600cd1ee12020f8
Vasily Mikhaylovsky <vasiliy.mikhaylovsky@gmail.com>
2015-10-15 20:12:58 +0500

    added get api key button

--- dasm/app/signup/controllers/signup-ctrl.js
+++ dasm/app/signup/controllers/signup-ctrl.js
@@ -1,11 +1,16 @@
 'use strict';
 angular.module('signup')
-.controller('SignupCtrl', function ($log, $state, $localForage, apiKey) {
-  this.submit = function () {
+.controller('SignupCtrl', function ($window, $state, $localForage, apiKey) {
+  var signup = this;
+  signup.getApiKey = function () {
+    var url = 'https://pddimp.yandex.ru/api2/registrar/get_token';
+    $window.open(url, '_system', 'location=yes');
+  };
+  signup.submit = function () {
     $localForage.setItem('apiKey', this.apiKey)
       .then(function () {
         return $state.go('main.domains');
       })
   };
-  this.apiKey = apiKey;
+  signup.apiKey = apiKey;
 });
