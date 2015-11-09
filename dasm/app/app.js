bd4e91f5c35098f3c1e97371900c16d0c0822ab3
Vasily Mikhaylovsky <vasiliy.mikhaylovsky@gmail.com>
2015-11-09 14:06:15 +0500

    added web version

--- dasm/app/app.js
+++ dasm/app/app.js
@@ -1,8 +1,19 @@
-'use strict';
+'use strict'
 angular.module('domainAsMail', [
   'main',
   'signup'
 ])
+.config(function ($locationProvider) {
+  if (/^https?:/.test(location.protocol)) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    })
+  }
+})
 .config(function ($ionicConfigProvider) {
-  $ionicConfigProvider.views.maxCache(0);
-});
+  $ionicConfigProvider.views.maxCache(0)
+})
+.config(function ($ionicConfigProvider) {
+  $ionicConfigProvider.scrolling.jsScrolling(false)
+})
--- dasm/app/main/services/pdd-serv.js
+++ dasm/app/main/services/pdd-serv.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 angular.module('main')
-.factory('PDDFactory', function ($http) {
-  var URL = 'https://pddimp.yandex.ru/api2/admin/';
+.factory('PDDFactory', function ($http, Config) {
+  var URL = Config.ENV.URL
   return function (apiKey) {
     var query = function (urlLocation, params) {
       var q = angular.extend({
@@ -9,43 +9,43 @@ angular.module('main')
         headers: {
           PddToken: apiKey
         }
-      }, params);
+      }, params)
       return $http(q)
       .then(function (result) {
         if (result.data.error) {
-          throw new Error(result.data.error);
+          throw new Error(result.data.error)
         }
         else {
-          return result.data;
+          return result.data
         }
-      });
-    };
+      })
+    }
     return {
       domain: {
         query: function () {
           return query('domain/domains')
         },
         register: function (domain) {
-          return query('domain/register', {params: {domain: domain}, method: 'POST'});
+          return query('domain/register', {params: {domain: domain}, method: 'POST'})
         }
       },
       email: {
         query: function (domain) {
-          return query('email/list', {params: {domain: domain, page: 1, 'on_page': 1000, direction: 'asc'}});
+          return query('email/list', {params: {domain: domain, page: 1, 'on_page': 1000, direction: 'asc'}})
         },
         addAlias: function (params) {
-          return query('email/add_alias', {params: params, method: 'POST'});
+          return query('email/add_alias', {params: params, method: 'POST'})
         },
         delAlias: function (params) {
-          return query('email/del_alias', {params: params, method: 'POST'});
+          return query('email/del_alias', {params: params, method: 'POST'})
         },
         addMailbox: function (params) {
-          return query('email/add', {params: params, method: 'POST'});
+          return query('email/add', {params: params, method: 'POST'})
         },
         removeMailbox: function (params) {
-          return query('email/del', {params: params, method: 'POST'});
+          return query('email/del', {params: params, method: 'POST'})
         }
       }
-    };
-  };
-});
+    }
+  }
+})
