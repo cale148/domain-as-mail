aa44154f5e0b15dbaecb9f181cee2a42a8a0dd51
Vasily Mikhaylovsky <vasiliy.mikhaylovsky@gmail.com>
2015-11-17 09:04:02 +0500

    pump version 0.4.2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose run --rm appm /build-release.sh /appm-release-key.keystore dasm $(cat keys/storekey) $(cat keys/keypassword) prod dasm.$(git describe --tags).apk && \
-mv dasm/dasm.v$(git describe --tags).apk ./
+mv dasm/dasm.$(git describe --tags).apk ./
