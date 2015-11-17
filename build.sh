0d8a1447fa01c58424c5e2cc9f57b411b57d975d
Vasily Mikhaylovsky <vasiliy.mikhaylovsky@gmail.com>
2015-11-17 18:25:12 +0500

    rename dasm to appm

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose run --rm appm /build-release.sh /appm-release-key.keystore dasm $(cat keys/storekey) $(cat keys/keypassword) prod dasm.$(git describe --tags).apk && \
-mv dasm/dasm.$(git describe --tags).apk ./
+mv appm/dasm.$(git describe --tags).apk ./
